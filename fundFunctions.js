2f61638d1cc825ee5cb61e370d3f3520c15aa415
Josef Picha <josefpicha28@centrum.cz>
2022-04-19 15:11:07 +0200

    change function name and attribute 04/19/2022 14:10

--- fundFunctions.js
+++ fundFunctions.js
@@ -12,22 +12,22 @@ console.log(object_matches({ hair: 'long', beard: true }, { age: 26, hair: 'long
 
 //#Source https://bit.ly/2neWfJ2
 
-const copyToClipboard = str => {
-    const el = document.createElement('textarea');
-    el.value = str;
-    el.setAttribute('readonly', '');
-    el.style.position = 'absolute';
-    el.style.left = '-9999px';
-    document.body.appendChild(el);
-    const selected =
-      document.getSelection().rangeCount > 0 ? document.getSelection().getRangeAt(0) : false;
-    el.select();
-    document.execCommand('copy');
-    document.body.removeChild(el);
-    if (selected) {
-      document.getSelection().removeAllRanges();
-      document.getSelection().addRange(selected);
-    }
+const copy_to_clipboard = string => {
+  const element = document.createElement('textarea');
+  element.value = string;
+  element.setAttribute('readonly', '');
+  element.style.position = 'absolute';
+  element.style.left = '-9999px';
+  document.body.appendChild(el);
+  const selected =
+    document.getSelection().rangeCount > 0 ? document.getSelection().getRangeAt(0) : false;
+  element.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+  if (selected) {
+    document.getSelection().removeAllRanges();
+    document.getSelection().addRange(selected);
+  }
 };
 
 //3. Write a JavaScript program to converts a comma-separated values (CSV) string to a 2D array.
