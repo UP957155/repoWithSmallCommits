83763baecc3d9de2d64d2e3fdd1b4ddd9ad72ffd
Josef Picha <josefpicha28@centrum.cz>
2022-04-19 14:41:19 +0200

    change attribute and variable 04/19/2022 13:41

--- mathFunctionsThird.js
+++ mathFunctionsThird.js
@@ -1,18 +1,18 @@
 //1. Write a JavaScript function that Convert an integer into a Roman Numeral in javaScript.
 
-function intToRom(num) {
-    if (typeof num !== 'number') 
+function intToRom(number) {
+    if (typeof numeric !== 'number') 
         return false; 
     
     var digits = String(+num).split(""),
     key = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM",
     "","X","XX","XXX","XL","L","LX","LXX","LXXX","XC",
     "","I","II","III","IV","V","VI","VII","VIII","IX"],
-    roman_num = "",
+    romanNum = "",
     i = 3;
     while (i--)
-    roman_num = (key[+digits.pop() + (i * 10)] || "") + roman_num;
-    return Array(+digits.join("") + 1).join("M") + roman_num;
+    romannum = (key[+digits.pop() + (i * 10)] || "") + romanNum;
+    return Array(+digits.join("") + 1).join("M") + romanNum;
 };
 console.log(intToRom(27));
 
