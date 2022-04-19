d463412472a22b42c8eed213888f23e37f9a940f
Josef Picha <josefpicha28@centrum.cz>
2022-04-19 15:05:55 +0200

    change function name and attribute 04/19/2022 14:05

--- mathFunctionsThird.js
+++ mathFunctionsThird.js
@@ -131,8 +131,8 @@ console.log(pos_to_neg(15));
 
 //10. Write a JavaScript function to cast a square root of a number to an integer.
 
-function sqrtToInt(num){
-    return parseInt(Math.sqrt(num)+"");
+function sqrt_to_int(number){
+    return parseInt(Math.sqrt(number)+"");
 };
-console.log(sqrtToInt(17));
+console.log(sqrt_to_int(17));
 
