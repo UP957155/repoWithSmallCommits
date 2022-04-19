4028ad05b7751bbe2586be271cb739d7b6da9e76
Josef Picha <josefpicha28@centrum.cz>
2022-04-19 15:08:19 +0200

    change function name and attribute 04/19/2022 14:06

--- fundFunctions.js
+++ fundFunctions.js
@@ -2,11 +2,11 @@
 
 //#Source https://bit.ly/2neWfJ2
 
-const matches = (obj, source) =>
-  Object.keys(source).every(key => obj.hasOwnProperty(key) && obj[key] === source[key]);
-console.log(matches({ age: 25, hair: 'long', beard: true }, { hair: 'long', beard: true })); // true
-console.log(matches({ hair: 'long', beard: true }, { age: 25, hair: 'long', beard: true })); // false
-console.log(matches({ hair: 'long', beard: true }, { age: 26, hair: 'long', beard: true })); // false
+const object_matches = (object, source) =>
+  Object.keys(source).every(key => object.hasOwnProperty(key) && object[key] === source[key]);
+console.log(object_matches({ age: 25, hair: 'long', beard: true }, { hair: 'long', beard: true })); // true
+console.log(object_matches({ hair: 'long', beard: true }, { age: 25, hair: 'long', beard: true })); // false
+console.log(object_matches({ hair: 'long', beard: true }, { age: 26, hair: 'long', beard: true })); // false
 
 //2. Write a JavaScript program to copy a string to the clipboard.
 
